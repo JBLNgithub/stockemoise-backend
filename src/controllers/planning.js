9450a8ee822165f7c6534f1899508cc84fbe8a79
JBLN <jashbalon@gmail.com>
2025-10-17 13:36:46 +0200

    implements planing model

--- src/controllers/planning.js
+++ src/controllers/planning.js
@@ -1,4 +1,14 @@
-export const nextEvents = (req, res) => {
-    console.log("TODO")
-    res.send("TODO")
+import {pool} from '../databases/SQLite3.js'
+import { nextEvents as nextEventsModel } from '../models/events.js'
+
+
+export const nextEvents = async(req, res) => {
+    try {
+        const events = await nextEventsModel(pool)
+        res.status(200).send(events)
+    }
+    catch(err) {
+        console.error(err)
+        res.sendStatus(500)
+    }
 }

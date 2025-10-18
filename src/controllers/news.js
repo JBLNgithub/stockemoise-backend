5e008328a08ecdcf9b0a2f47cf7d0fa5c97e0824
JBLN <jashbalon@gmail.com>
2025-10-18 11:00:18 +0200

    implements models of basic requests

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -1,5 +1,10 @@
 import {pool} from '../databases/SQLite3.js'
-import { allNews } from '../models/news.js'
+import { 
+    allNews,
+    getSingleNews as getSingleNewsModel,
+    getSingleEventNews,
+    nextEventNews as nexEventNewsModel
+ } from '../models/news.js'
 
 
 export const getAllNews = async(req, res) => {
@@ -13,14 +18,34 @@ export const getAllNews = async(req, res) => {
     }
 }
 
-export const getSingleNews = (req, res) => {
-    console.log("TODO get")
-    res.send("TODO")
+export const getSingleNews = async(req, res) => {
+    const {id} = req.params
+    try {
+        const news = await getSingleNewsModel(pool, id)
+
+        if(!news) throw new Error("news does not exist")
+
+        const eventNews = await getSingleEventNews(pool, id)
+
+        news.event = eventNews ? eventNews : null;
+
+        res.status(200).send(news)
+    }
+    catch(err) {
+        console.error(err)
+        res.sendStatus(500)
+    }
 }
 
-export const nextEventNews = (req, res) => {
-    console.log("TODO next")
-    res.send("TODO")
+export const nextEventNews = async(req, res) => {
+    try {
+        const eventNews = await nexEventNewsModel(pool, req.query.limit)
+        res.status(200).send(eventNews)
+    }
+    catch(err) {
+        console.error(err)
+        res.sendStatus(500)
+    }
 }
 
 export const addNews = (req, res) => {
--- src/models/news.js
+++ src/models/news.js
@@ -4,3 +4,30 @@ export const allNews = async(SQLClient) => {
     const rows = await SQLClient.query(query)
     return rows
 }
+
+export const getSingleNews = async(SQLClient, id) => {
+    const query = 'SELECT * FROM news WHERE id = $1'
+
+    const rows = await SQLClient.query(query, [id])
+    return rows[0]
+}
+
+export const getSingleEventNews = async(SQLClient, id) => {
+    const query = 'SELECT * FROM eventNews WHERE id = $1'
+
+    const rows = await SQLClient.query(query, [id])
+    return rows[0]
+}
+
+export const nextEventNews = async(SQLClient, limit) => {
+    let query = "SELECT * FROM eventNews en INNER JOIN news n ON en.id = n.id WHERE en.dateEvent > datetime('now') ORDER BY en.dateEvent ASC"
+    const queryValues = []
+
+    if(limit) {
+        query += ' LIMIT $1'
+        queryValues.push(limit)
+    }
+
+    const rows = await SQLClient.query(query, queryValues)
+    return rows
+}

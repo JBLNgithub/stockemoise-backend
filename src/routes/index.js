3f8223625e7bb0823cc335495f42495d720256e9
JBLN <jashbalon@gmail.com>
2025-11-03 19:15:06 +0100

    add script to build

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,22 @@
 import {Router} from 'express'
 import APIRouter from './API.js'
 import docs from './api-docs.js'
+import express from 'express'
+import path from 'path'
 
 
 const router = Router()
 
+// backend
 router.use('/api', APIRouter)
 router.use('/api-docs', docs)
+
+// frontend
+const __dirname= path.resolve()
+router.use(express.static(path.join(__dirname, "/dist")))
+router.get('*all', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"))
+})
+
 
 export default router

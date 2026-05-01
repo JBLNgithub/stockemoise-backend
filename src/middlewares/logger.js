7a16cdf2c14a9b29e32aa417057d8ab6fa65a0f7
JBLN <jashbalon@gmail.com>
2026-05-01 13:56:47 +0200

    Improve logger

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,6 +1,30 @@
+import {format} from 'date-fns'
+import path from 'path'
+import fs from 'fs'
+
+
 const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`)
+    // to file log
+    // console.log(`${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}\t${req.method}\t${req.headers.origin}\t${req.url}`)
+    
+    // to console log
+    console.log(`${req.method}\t${req.headers.origin}\t${req.url}`)
     next()
+}
+
+const errLog = async(message) => {
+    const logItem = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}\t${message}`
+
+    try {
+        if(!fs.existsSync(path.join(__dirname), '..', 'logs')) {
+            await fs.promises.mkdir(path.join(__dirname, '..', 'logs'))
+        }
+        await fs.promises.appendFile(path.join(__dirname, '..', 'logs', 'errLog.txt'), logItem)
+    }
+    catch(err) {
+        console.error(err)
+    }
+
 }
 
 
